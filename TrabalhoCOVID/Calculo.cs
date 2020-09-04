925ea62f098e1ab01a8444893e5250f5caf4adb6
Reinaldo Matis Gussi <reinaldo.gussi@hbsis.com.br>
2020-09-04 09:16:16 -0300

    Add calculo

--- TrabalhoCOVID/Calculo.cs
+++ TrabalhoCOVID/Calculo.cs
@@ -10,20 +10,10 @@ namespace TrabalhoCOVID
 {
     public class Calculo : Monitoramento
     {
-        public int _total { get; set; }
-        //public int _tempo { get; set; }
 
         public Calculo()
         {
-            //_total = Total(base._estados, base._pessoas);
             Ret(base._estados, base._pessoas);
-           // _tempo = 0;
-
-        }
-
-        public static int Total(int estados, int pessoas)
-        {
-            return estados * pessoas;
         }
 
         public static int Rnd(int limite)
@@ -34,20 +24,15 @@ namespace TrabalhoCOVID
 
         public static void Ret(int estados, int pessoas)
         {
-            int total = Total(estados, pessoas);
-            Random rnd = new Random();
-            int ale = 0;
             int linha = 0;
-            int e = 0;
+            int e;
             int contador = 0;
             bool titi = true;
             string[] pes = new string[estados];
-            int[] infectados = new int[estados];
             int tempo = 0;
             Console.Clear();
             for (int es = 0; es < estados; es++)
             {
-
                 for (int p = 0; p < pessoas; p++)
                 {
                     pes[es] += "-";
@@ -55,49 +40,54 @@ namespace TrabalhoCOVID
             }
             while (tempo < 60)
             {
-                 e = rnd.Next(estados);
-                //for(int e = 0; e < estados; e++)
-                //{
-                    
-                    for(int p = 0; p < pessoas; p++)
-                    {
-                        
-                        //pess[e] = Regex.Replace(pes[e], @"-" , "*");
-
-                    }
+                 e = Rnd(estados);                 
                     if (!titi)
                     {
-                        StringBuilder pess = new StringBuilder(pes[e]);
-                        ale = rnd.Next(pessoas);
-                        if (pess[ale] == '-')
-                        {
-                            linha = e;
-                            pess[ale] = '*';
-                            pes[e] = pess.ToString();
-                            contador++;
-                            titi = true;
-                        }
+                    pes[e] = Infectar(pes[e], pessoas, e,contador, out titi, out linha, out contador);
                     }
 
                 for (var i = 0; i < estados; i++)
                 {
                     if (i == linha)
-                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Red;                  
                     Console.WriteLine(pes[i]);
                     Console.ResetColor();
                 }
-               // }
-                
+
+                Console.WriteLine("\n\n\n Runtime: " + tempo + "s");
                 Thread.Sleep(1000);
-                infectados[rnd.Next(estados)]++;
                 tempo++;
                 titi = false;
+                
                 Console.Clear();
             }
             Console.WriteLine("TEMPO DE ANALISE FINALIZADO");
             Console.WriteLine("Pessoas infectadas: " + contador);
         }
 
+       public static string Infectar(string pessoas, int pessoa,int estado, int cotad, out bool titis, out int linhas, out int contador)
+        {
+            
+            StringBuilder pess = new StringBuilder(pessoas);
+            int ale = Rnd(pessoa);
+            if (pess[ale] == '-')
+            {
+                Linha(estado, out linhas);
+                pess[ale] = '*';
+                titis = true;
+                contador = cotad + 1;
+                return pess.ToString();              
+            }
+            contador = cotad + 0;
+            titis = false;
+            linhas = estado+1;
+            return pessoas;
+        }
+
+        public static void Linha(int estado, out int linha)
+        {
+            linha = estado;
+        }
 
 
 
